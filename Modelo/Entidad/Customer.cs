b73f84317d2069e3965bfc3437b41dfa49191d63
Elianb7 <61747736+Elianb7@users.noreply.github.com>
2022-01-24 09:54:30 -0500

    Add files via upload

---
+++ Modelo/Entidad/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion.Entidad
+{
+    class Customer
+    {
+        public int Customer_Id { get; set; }
+        public int Store_Id { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Email { get; set; }
+        public int Address_id { get; set; }
+        public string Activebool { get; set; }
+        public int Create_date { get; set; }
+        public string Last_update { get; set; }
+        public string Active { get; set; }
+    }
+}
