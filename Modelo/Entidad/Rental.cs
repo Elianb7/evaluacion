5058592a0b27483c1fbd603a37bfd54ab7864bea
Elianb7 <61747736+Elianb7@users.noreply.github.com>
2022-01-25 08:23:36 -0500

    Update Rental.cs

--- Modelo/Entidad/Rental.cs
+++ Modelo/Entidad/Rental.cs
@@ -8,5 +8,12 @@ namespace Evaluacion.Entidad
 {
     class Rental
     {
+        public int Rental_Id { get; set; }
+        public string Rental_Date { get; set; }
+        public int Inventory_Id { get; set; }
+        public int Customer_Id { get; set; }
+        public string Return_Date { get; set; }
+        public int Staff_Id { get; set; }
+        public string Last_update { get; set; }
     }
 }
